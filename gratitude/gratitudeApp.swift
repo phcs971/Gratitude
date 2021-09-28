9f607a699060b9ac2f0e5d46b77b484817cf95ee
phcs971 <phcs.971@gmail.com>
2021-09-28 14:18:59 -0300

    Initial Commit

---
+++ gratitude/gratitudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gratitudeApp.swift
+//  gratitude
+//
+//  Created by Pedro Henrique Cordeiro Soares on 28/09/21.
+//
+
+import SwiftUI
+
+@main
+struct gratitudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
